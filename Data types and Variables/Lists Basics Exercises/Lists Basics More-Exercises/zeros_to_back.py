134bfbd64c7c0089afc71fa6d852b29957e4ccce
Milen <feniks9511@gmail.com>
2022-06-08 14:45:51 +0300

    added exercise

---
+++ Data types and Variables/Lists Basics Exercises/Lists Basics More-Exercises/zeros_to_back.py
@@ -0,0 +1,9 @@
+single_string = input().split(", ")
+
+for element in single_string:
+    if element == "0":
+        element_index = single_string.index(element)
+        single_string.append(element)
+        single_string.pop(element_index)
+
+print(single_string)
