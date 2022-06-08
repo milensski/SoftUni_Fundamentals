0ae150106e5e7d7534a8faeb1f2207a70b15e5e6
Milen <feniks9511@gmail.com>
2022-06-08 15:55:03 +0300

    added exercise

---
+++ Data types and Variables/Lists Basics Exercises/Lists Basics More-Exercises/messaging.py
@@ -0,0 +1,18 @@
+sequence_of_numbers = input().split()
+chars = input()
+
+print(sequence_of_numbers)
+
+queue_of_sums = []
+
+for sequence in sequence_of_numbers:
+    sum_of_sequence = 0
+    for element in sequence:
+        sum_of_sequence += int(element)
+    #sum = 29
+
+    while True:
+
+
+
+print(queue_of_sums)
