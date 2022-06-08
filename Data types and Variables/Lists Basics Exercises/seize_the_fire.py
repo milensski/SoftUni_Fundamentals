1536e48c7c9d05b683834fc26a9fefb671949fde
Milen <feniks9511@gmail.com>
2022-06-08 10:24:38 +0300

    added exercise

--- Data types and Variables/Lists Basics Exercises/seize_the_fire.py
+++ Data types and Variables/Lists Basics Exercises/seize_the_fire.py
@@ -4,6 +4,10 @@ cells = []
 type_of_fire = []
 value_of_fire = []
 
+valid_cells = []
+total_effort = 0
+total_fire = 0
+
 for cell in fires:
     cells.append(cell.split(" = "))
 
@@ -18,4 +22,24 @@ for index in range(len(type_of_fire)):  # print(f"{type_of_fire[index]} = {value
 
     # WORKING WITH EACH CELL:
 
+    if current_type == "High" and 81 <= current_value <= 125 and current_value <= water:
+        valid_cells.append(int(current_value))
+        water -= current_value
+        total_fire += current_value
+        total_effort += current_value * 0.25
+    elif current_type == "Medium" and 51 <= current_value <= 80 and current_value <= water:
+        valid_cells.append(int(current_value))
+        water -= current_value
+        total_fire += current_value
+        total_effort += current_value * 0.25
+    elif current_type == "Low" and 1 <= current_value <= 50 and current_value <= water:
+        valid_cells.append(int(current_value))
+        water -= current_value
+        total_fire += current_value
+        total_effort += current_value * 0.25
 
+print(f"Cells: ")
+for valid in valid_cells:
+    print(f" - {valid}")
+print(f"Effort: {total_effort:.2f}")
+print(f"Total Fire: {total_fire}")
