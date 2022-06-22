2d0cfd88254c6160b762c6c11558242cdbb0e00e
Milen <feniks9511@gmail.com>
2022-06-22 20:51:13 +0300

    Create groups_of_tens.py

---
+++ Lists Advanced Exercises/groups_of_tens.py
@@ -0,0 +1,17 @@
+sequence = input().split(", ")
+sequence = list(map(int, sequence))
+
+new_list = []
+group_counter = 10
+
+while 0<len(sequence):
+    for element in sequence:
+        if element <= group_counter:
+            new_list.append(element)
+    print(f"Group of {group_counter}'s: {new_list}")
+    group_counter += 10
+
+    for num in new_list:
+        if num in sequence:
+            sequence.remove(num)
+    new_list.clear()
