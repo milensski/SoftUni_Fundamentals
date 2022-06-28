47f9ea58c8516e78eb1331b050b1fe4a54d54954
Milen <feniks9511@gmail.com>
2022-06-28 10:25:25 +0300

    added exercise

--- Classes_and_Objects/zoo.py
+++ Classes_and_Objects/zoo.py
@@ -1,22 +1,22 @@
 class Zoo:
     __animals = 0
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.mammals = []
         self.fishes = []
         self.birds = []
 
-    def add_animal(self,species,name):
+    def add_animal(self, species, name):
         if species == "mammal":
             self.mammals.append(name)
         elif species == "fish":
             self.fishes.append(name)
         elif species == "bird":
             self.birds.append(name)
-        Zoo.__animals +=1
+        Zoo.__animals += 1
 
-    def get_info(self,species):
+    def get_info(self, species):
         if species == "mammal":
             print(f"Mammals in {self.name}: {', '.join(self.mammals)}")
         elif species == "fish":
@@ -37,10 +37,8 @@ for i in range(n):
     species = line[0]
     animal = line[1]
 
-    zoo.add_animal(species,animal)
+    zoo.add_animal(species, animal)
 
 specie = input()
 
 zoo.get_info(specie)
-
-
