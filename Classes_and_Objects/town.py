0bd9ce78bcfadff809568445177fee16a0081d22
Milen <feniks9511@gmail.com>
2022-06-29 13:42:58 +0300

    added exercise

---
+++ Classes_and_Objects/town.py
@@ -0,0 +1,21 @@
+class Town:
+
+    def __init__(self, name):
+        self.name = name
+        self.latitude = "0°N"
+        self.longitude = "0°E"
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
+
+
+town = Town("Sofia")
+town.set_longitude("West")
+town.set_latitude("Middle")
+print(town)
