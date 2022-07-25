f3ff5c12b8ce2d71f8ab2685c096695aa02aa2b9
Milen <feniks9511@gmail.com>
2022-07-25 11:33:57 +0300

    added exercise

--- Final Exam Preperation/ad_astra.py
+++ Final Exam Preperation/ad_astra.py
@@ -2,18 +2,15 @@ import re
 
 text = input()
 
+pattern = r'([\#|\|])([a-zA-Z ]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1'
 
-pattern = r'([?<=#|\|])([a-zA-Z ]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1'
-
-result = re.finditer(pattern,text)
+result = re.finditer(pattern, text)
 
 calories = 0
 
 for match in result:
     if match.group() is not None and match.group(2).strip() != "":
         calories += int(match.group(4))
-
-
 days = calories // 2000
 
 print(f"You have food to last you for: {days} days!")
@@ -21,7 +18,7 @@ print(f"You have food to last you for: {days} days!")
 if days == 0:
     exit()
 
-result = re.finditer(pattern,text)
+result = re.finditer(pattern, text)
 
 for match in result:
     products = match.group(2)
--- RegEx/Exercises/find_variable_in_sentence.py
+++ RegEx/Exercises/find_variable_in_sentence.py
@@ -1,15 +1,11 @@
 import re
 
-user_pattern = r'( |^)[A-Za-z0-9]+((\.|\-|\_)[A-Za-z0-9]+)*'
-host_pattern = r'[a-zA-Z]+(-([a-zA-z]+))*(\.[a-zA-Z]+(-([a-zA-z]+)\.[a-zA-Z]+)*)+'
-
-search_pattern = fr'{user_pattern}@{host_pattern}'
+search_pattern = r'(?<=\_)\w+'
 
 
 text = input()
 
-result = re.finditer(search_pattern,text)
+result = re.findall(search_pattern,text)
 
 for match in result:
-    print(match.group())
-
+    print(match.group(),end=",")
